e026921c607e6d55123ae7752719c4cf28730690
Дмитрий Фирсов <firsov.2008@mail.ru>
2023-12-16 19:59:32 +0300

    Небольшие изменения и добавление комментариев

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import random
 pygame.init()
 size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
-mouse_x, mouse_y = 0, 0
 all_sprites = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
+    '''
+    Функция для загрузки изображений
+    '''
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -27,7 +29,10 @@ def load_image(name, colorkey=None):
     return image
 
 
-def blitRotate(surf, image, pos, originPos, angle):
+def blit_rotate(surf, image, pos, originPos, angle):
+    '''
+    Поворот спрайтов относительно центра
+    '''
     image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1] - originPos[1]))
     offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
 
@@ -43,23 +48,44 @@ def blitRotate(surf, image, pos, originPos, angle):
 
 
 def distance(x1, y1, x2, y2):
+    '''
+    Расчет расстояния между точками
+    '''
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 
 class Camera:
+    '''
+    Камера
+    '''
+
     def __init__(self, x, y, attached_entity=None):
+        # Координаты камеры
         self.x, self.y = x, y
+        # Привязанная сущность
+        # Камера будет "следить" за ней
         self.attached_entity = attached_entity
 
     def update(self):
+        '''
+        Обновление камеры
+        '''
         if self.attached_entity is not None:
             self.x, self.y = self.attached_entity.x, self.attached_entity.y
 
-    def draw(self, screen, obj):
-        obj.draw(screen, self.x, self.y)
+    def draw(self, screen, objs):
+        '''
+        Рисование объектов
+        '''
+        for obj in objs:
+            obj.draw(screen, self.x, self.y)
 
 
 class Entity(pygame.sprite.Sprite):
+    '''
+    Сущность
+    '''
+
     def __init__(self, x, y, direction=0, health=100):
         super().__init__()
         # Положение в пространстве
@@ -69,33 +95,58 @@ class Entity(pygame.sprite.Sprite):
         self.health = health
 
     def coords(self):
+        '''
+        Функция возвращает координаты сущности
+        '''
         return self.x, self.y
 
 
 class Tank(Entity):
+    '''
+    Танк
+    '''
+
     def __init__(self, x, y, direction=0, health=100, speed=1.5, ai='player'):
         super().__init__(x, y, direction, health)
+        # Загрузка изображений
         self.image_track = pygame.transform.scale(load_image("tank_track.png"), (128, 128))
         self.image_turret = pygame.transform.scale(load_image("tank_turret.png"), (128, 128))
+        # Задаем направление башни танка
         self.turret_direction = direction
+        # Скорость танка
         self.speed = speed
+        # ИИ
         self.ai = ai
 
     def move(self, multiplier=1):
+        '''
+        Перемещение танка
+        '''
         self.x += math.cos(math.radians(self.direction)) * self.speed * multiplier
         self.y += math.sin(math.radians(self.direction)) * self.speed * multiplier
 
     def turn(self, angle):
+        '''
+        Поворот танка
+        '''
         self.direction += angle
 
     def draw(self, screen, camera_x, camera_y):
+        '''
+        Рисование танка
+        '''
+        # Вычисляем координаты танка на экране при центре в (0, 0)
         x = (camera_x - self.x) * -1
         y = (camera_y - self.y) * -1
+        # Получаем координаты цели
         target_x, target_y = self.get_target()
+        # Вычисляем координаты цели относительно танка
         target_x, target_y = target_x - x, target_y - y
         try:
+            # Пробуем вычислить угол поворота башни танка
             target_angle = math.degrees(math.atan(target_y / target_x))
         except ZeroDivisionError:
+            # И если x оказался равен 0, то сами подставляем значения
             if target_y > 0:
                 target_angle = 90
             elif target_y < 0:
@@ -103,54 +154,87 @@ class Tank(Entity):
             else:
                 target_angle = 0
         else:
+            # А если нам удалось вычислить угол, то поворачиваем его на 180 градусов, если цель с другой стороны
             if target_x < 0:
                 target_angle += 180
-
-        blitRotate(screen, self.image_track, (x + width // 2, y + height // 2), (64, 64),
-                   self.direction * -1)
-        blitRotate(screen, self.image_turret, (x + width // 2, y + height // 2), (64, 64),
-                   target_angle * -1)
+        # Рисуем
+        blit_rotate(screen, self.image_track, (x + width // 2, y + height // 2), (64, 64),
+                    self.direction * -1)
+        blit_rotate(screen, self.image_turret, (x + width // 2, y + height // 2), (64, 64),
+                    target_angle * -1)
 
     def get_target(self):
+        '''
+        Получаем цель танка
+        '''
         if self.ai == 'player':
-            return mouse_x - width // 2, mouse_y - height // 2
+            # Если танком управляет игрок, то функция возвращает координаты мыши
+            x, y = pygame.mouse.get_pos()
+            return x - width // 2, y - height // 2
         elif self.ai == 'enemy':
+            # Если танком управляет враг, то функция возвращает координаты ближайшего игрока
             return sorted(filter(lambda sprite: type(sprite) is Tank and sprite.ai == 'player', all_sprites),
                           key=lambda sprite: distance(self.x, self.y, sprite.x, sprite.y))[0].coords()
         else:
+            # Если кто-то другой, то 0, 0
             return 0, 0
 
 
 class Box(Entity):
+    '''
+    Коробка
+    '''
+
     def __init__(self, x, y, direction=0):
         super().__init__(x, y, direction, -1)
         self.box = pygame.transform.scale(load_image("box.png"), (128, 128))
 
     def draw(self, screen, camera_x, camera_y):
+        '''
+        Рисование коробки
+        '''
+        # Вычисляем координаты коробки на экране при центре в (0, 0)
         x = (camera_x - self.x) * -1
         y = (camera_y - self.y) * -1
-        blitRotate(screen, self.box, (x + width // 2, y + height // 2), (64, 64),
-                   self.direction * -1)
+        # Рисуем
+        blit_rotate(screen, self.box, (x + width // 2, y + height // 2), (64, 64),
+                    self.direction * -1)
 
 
 if __name__ == '__main__':
-    screen.fill((0, 0, 0))
+    # Травяной цвет)
+    screen.fill((93, 161, 48))
+    # Создаем 10 коробок, чтобы видеть перемещения танка
     for _ in range(10):
         all_sprites.add(Box(random.randint(-300, 300), random.randint(-300, 300)))
+
+    # Создаем танк игрока
     tank = Tank(0, 0)
+    # Создаем танк врага
     enemy_tank = Tank(40, 40, ai='enemy')
+
+    # Создаем камеру
     camera = Camera(0, 0, tank)
+
+    # Добавляем танки во группу спрайтов
     all_sprites.add(tank)
     all_sprites.add(enemy_tank)
+
+    # Часы
     clock = pygame.time.Clock()
+
+    # Основной цикл
     running = True
     while running:
+        # Проходимся по ивентам
         for event in pygame.event.get():
+            # Если окно закрыли, то завершаем цикл
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEMOTION:
-                mouse_x, mouse_y = event.pos
+
+        # Получаем кнопки, которые нажаты
         keys = pygame.key.get_pressed()
+        # Управление на WASD
         if keys[pygame.K_w]:
             tank.move()
         if keys[pygame.K_s]:
@@ -160,12 +244,16 @@ if __name__ == '__main__':
         if keys[pygame.K_d]:
             tank.turn(1.5)
 
+        # Обновляем камеру
         camera.update()
 
-        screen.fill((0, 0, 0))
-        for sprite in all_sprites:
-            camera.draw(screen, sprite)
+        # Рисуем все что надо
+        screen.fill((93, 161, 48))
+        camera.draw(screen, all_sprites)
 
+        # Обновляем экран
         pygame.display.flip()
+
+        # Ждем следующий кадр
         clock.tick(60)
 pygame.quit()
