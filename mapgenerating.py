535eba5ede90fb18839da380b69207a2c2877ea5
shergeu <serzh.ilyukin@bk.ru>
2023-12-23 13:14:00 +0300

    Добавил метод draw_field для рисования карты, изменил метод для обновления кадров для карты uodate

--- mapgenerating.py
+++ mapgenerating.py
@@ -1,38 +1,43 @@
 import random
 import pygame
-from map_1 import Map_1
-from Test import load_image
+import os
+import sys
+
 """# - brick_barrier
    . - floor
    X - box"""
 
-MAP_SIZE = width_field, height_field = 1600, 900
+MAP_SIZE = width_field, height_field = 1000, 1000
 COEF = 50
 MAPS = ['data/map1.txt']
 
+def load_image(name, colorkey=None):
+    '''
+    Функция для загрузки изображений
+    '''
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 class Maps:
-    def __init__(self):
+    def __init__(self, main_screen):
+        self.screen = main_screen
         self.field = pygame.Surface((width_field, height_field))
-        self.width = 10
-        self.height = 10
         self.cell_size = 100
-       # self.x = 0
-       # self.y = 0
-       # self.screen = screen
-       # maps = [Map_1(self.screen)]
-       # self.map = random.choice(maps)
 
-#
+    def update(self, coords):
+        self.screen.blit(self.field, (-coords[0], -coords[1]))
 
-    def render_selected_map(self):
-        self.map.create_board()
-
-    def navigation(self, screen, coords):
-        s = screen
-        s.blit(self.screen, (-coords[0], -coords[1]))
-
-    def getb(self):
-        return self.map
 
     def select_random(self):
         choiced_map_txt = random.choice(MAPS)
@@ -40,33 +45,32 @@ class Maps:
             reading_map = map.readlines()
             self.textures = reading_map[-1].split(';')
             self.map = [line.strip('\n\r') for line in reading_map[:-1]]
-           # print(reading_map)
-           # print(self.map)
-           # print(self.textures)
+            self.width_in_tiles = len(self.map[0])
+            self.height_in_tiles = len(self.map)
+
+
     def generate(self):
         self.load_textures()
+        self.draw_field()
 
 
     def load_textures(self):
         if 'box.png' in self.textures:
             self.box = pygame.transform.scale(load_image("box.png"), (100, 100))
         if 'sandfloor.png' in self.textures:
-            self.sandfloor = pygame.transform.scale(load_image("sandfloor.png"), (80, 100))
+            self.sandfloor = pygame.transform.scale(load_image("sandfloor.png"), (100, 100))
         if 'brick_barrier.png' in self.textures:
             self.barrier = pygame.transform.scale(load_image("brick_barrier.png"), (100, 100))
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def draw_field(self):
+        for x in range(0, self.width_in_tiles):
+            for y in range(0, self.height_in_tiles):
+                if self.map[y][x] == '#':
+                    self.field.blit(self.barrier, (x * self.cell_size, y * self.cell_size))
+                if self.map[y][x] == 'X':
+                    self.field.blit(self.box, (x * self.cell_size, y * self.cell_size))
+                if self.map[y][x] == '.':
+                    self.field.blit(self.sandfloor, (x * self.cell_size, y * self.cell_size))
 
 #import pygame
 #import os
