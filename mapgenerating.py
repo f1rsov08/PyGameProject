d5e35a0777e106f0b57644e91929950522a54a5f
shergeu <serzh.ilyukin@bk.ru>
2023-12-23 13:20:52 +0300

    Добавил метод create_size_map для создания плоскости surface под размер карты

--- mapgenerating.py
+++ mapgenerating.py
@@ -7,9 +7,8 @@ import sys
    . - floor
    X - box"""
 
-MAP_SIZE = width_field, height_field = 1000, 1000
-COEF = 50
 MAPS = ['data/map1.txt']
+
 
 def load_image(name, colorkey=None):
     '''
@@ -29,15 +28,13 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+
 class Maps:
     def __init__(self, main_screen):
         self.screen = main_screen
-        self.field = pygame.Surface((width_field, height_field))
-        self.cell_size = 100
 
     def update(self, coords):
         self.screen.blit(self.field, (-coords[0], -coords[1]))
-
 
     def select_random(self):
         choiced_map_txt = random.choice(MAPS)
@@ -47,12 +44,16 @@ class Maps:
             self.map = [line.strip('\n\r') for line in reading_map[:-1]]
             self.width_in_tiles = len(self.map[0])
             self.height_in_tiles = len(self.map)
+            self.create_size_map()
 
+    def create_size_map(self):
+        self.cell_size = 100
+        width_field, height_field = self.cell_size * self.width_in_tiles, self.cell_size * self.height_in_tiles
+        self.field = pygame.Surface((width_field, height_field))
 
     def generate(self):
         self.load_textures()
         self.draw_field()
-
 
     def load_textures(self):
         if 'box.png' in self.textures:
@@ -71,48 +72,3 @@ class Maps:
                     self.field.blit(self.box, (x * self.cell_size, y * self.cell_size))
                 if self.map[y][x] == '.':
                     self.field.blit(self.sandfloor, (x * self.cell_size, y * self.cell_size))
-
-#import pygame
-#import os
-#import sys
-#
-#class Board:
-#    # создание поля
-#    def __init__(self, width, height):
-#        self.width = width
-#        self.height = height
-#        self.board = [[0] * width for _ in range(height)]
-#        # значения по умолчанию
-#        self.left = 25
-#        self.top = 25
-#        self.cell_size = 40
-#
-#    # настройка внешнего вида
-#    def set_view(self, left, top, cell_size):
-#        self.left = left
-#        self.top = top
-#        self.cell_size = cell_size
-#
-#    def render(self, name_screen):
-#        for x in range(0, self.width):
-#            for y in range(0, self.height):
-#                pygame.draw.rect(name_screen, 'white', (self.left + x * self.cell_size, self.top + y * self.cell_size,
-#                                                        self.cell_size, self.cell_size), 1)
-#
-#
-#if __name__ == '__main__':
-#    pygame.init()
-#    size = width, height = 600, 600
-#    screen = pygame.display.set_mode(size)
-#
-#    running = True
-#    board = Board(14, 14)
-#    while running:
-#        for event in pygame.event.get():
-#            if event.type == pygame.QUIT:
-#                running = False
-#        screen.fill((0, 0, 0))
-#        board.render(screen)
-#        all_sprites.draw(screen)
-#        pygame.display.flip()
-#    pygame.quit()
