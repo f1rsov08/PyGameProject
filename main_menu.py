f2a04f5817e19fedb01eb7d7ba2c4f00fcd07765
shergeu <serzh.ilyukin@bk.ru>
2024-01-13 17:14:17 +0300

    сделал __init__

--- main_menu.py
+++ main_menu.py
@@ -1,4 +1,34 @@
 import pygame
 
+pygame.init()
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size)
+
+
 class Button:
-    pass
+    def __init__(self, size, pos, color, text, color_text, size_font):
+        self.text = text
+        self.color_text = color_text
+        self.input_text = text
+        self.color = color
+        self.width = size[0]
+        self.height = size[1]
+        self.font_size = size_font
+        self.x = pos[0]
+        self.y = pos[1]
+
+
+
+clock = pygame.time.Clock()
+running = True
+screen.fill('green')
+#but = Button((200, 100), (100, 100), 'black', "Играть", 'white', 40)
+#but.create()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #but.update()
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
